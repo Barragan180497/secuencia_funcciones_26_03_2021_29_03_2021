ab1566e2e89a3a9b5f5a7ed8c84dc0f0b0726e53
Barragan180497 <fabaes18@hotmail.com>
2021-03-30 10:29:23 -0500

    Clase_26_03_2021_29_03_2021

---
+++ main.js
@@ -0,0 +1,22 @@
+// Codigo en funcion
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+
+Saludar("Miguel Angel");
+Saludar("Marly");
+Saludar("Orlando");
+Saludar("Fabian Andres Barragan Espinosa");
+
+
+// Codigo ens ecuencia
+let saludar;
+saludar = "Miguel Angel";
+console.warn(`Hola ${saludar}`);
+saludar = "Marly";
+console.warn(`Hola ${saludar}`);
+saludar = "Orlando";
+console.warn(`Hola ${saludar}`);
+saludar = "Fabian";
+console.warn(`Hola ${saludar}`);
